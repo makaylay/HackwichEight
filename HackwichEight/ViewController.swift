c05f6d35cdec539076e70b6cc93fc3b6ade55e67
CM Student <cmstudent@uhwo-acm-12.local>
2018-04-02 10:48:26 -1000

    Added three annotations on map

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     //set initial location to UHWO
     let initialLocation = CLLocation(latitude: 21.361888, longitude: -158.055725)
     let restaurantOne = CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098)
+    let restaurantTwo = CLLocationCoordinate2D(latitude: 21.338298, longitude: -158.077973)
+    let restaurantThree = CLLocationCoordinate2D(latitude: 21.339880, longitude: -158.078137)
     //set region around UHWO to 1000m
     let regionRadius: CLLocationDistance = 10000
     
@@ -28,6 +30,10 @@ class ViewController: UIViewController {
         //create an Restaurant object and set all the arguments to each of the three parameters
         let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
         mapView.addAnnotation(restaurantOne)
+        let restaurantTwo = Restaurant(title: "My Cafe", type: "Comfort Food", coordinate: CLLocationCoordinate2D(latitude: 21.338298, longitude: -158.077973))
+        mapView.addAnnotation(restaurantTwo)
+        let restaurantThree = Restaurant(title: "Sushi Bay", type: "Japanese", coordinate: CLLocationCoordinate2D(latitude: 21.339880, longitude: -158.078137))
+        mapView.addAnnotation(restaurantThree)
     }
     
     func centerMapOnLocation(location: CLLocation) {
